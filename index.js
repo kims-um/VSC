ab91b3b24f70a574bb46ae842ac7af39485c0a64
kims-um <kims.minsu@gmail.com>
2020-12-03 21:01:51 +0900

    update README.md

---
+++ index.js
@@ -0,0 +1,14 @@
+const pig = {
+    name : "Bohye",
+    age : 23,
+    weight: "65kg",
+    fatgirl : true,
+    favfood : ["Kims", "Kiss", "Sex"],
+    favphoto : [{
+        name : "Gun",
+        farry: true
+    }]
+
+}
+
+console.log(pig)
